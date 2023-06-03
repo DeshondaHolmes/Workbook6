0e44e779d8904addb7adabef28065eca05f0de8e
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-03 12:58:44 -0400

    Add type method to python code

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -9,7 +9,33 @@ def greet():   #function name greet
 print(greet())
 
 
+"""
+def greet():   #function name greet 
+    msg = "Hello world!"
+    return msg
+print(greet())
+Comments this does not show
+"""
 
 
+print("-------------------")
+#declare variables 
+
+#declare stings
+cat = "mick"
+age = "48965"
+print("My cat name is " + cat + "and is " + age + " years old.")
+
+print("-------------------")
+
+
+
+print(type(cat))
+
+"""
+Using type() you can find variable data type
+<class 'str">
+
+"""
 
  
